280a1854324a2f4e5449c9586bce501de2576435
Nico Stegmann <nico.stegmann@hotmail.de>
2024-08-16 14:56:31 +0200

    new

---
+++ src/Aufgaben/strings/A16_Palindrom.java
@@ -0,0 +1,19 @@
+package aufgaben.strings;
+
+import java.util.Scanner;
+
+public class A16_Palindrom {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String text = input.nextLine();
+
+        System.out.println(text.trim());
+
+
+
+    }
+
+}
