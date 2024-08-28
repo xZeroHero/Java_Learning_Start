9dc33b74b939feee4f6080118e3f4d3d941ab752
Nico Stegmann <nico.stegmann@hotmail.de>
2024-08-28 15:04:20 +0200

    update

--- src/Aufgaben/teams_aufgaben/A28_Klassenspiegel_Methode.java
+++ src/Aufgaben/teams_aufgaben/A28_Klassenspiegel_Methode.java
@@ -1,2 +1,39 @@
-package aufgaben.teams_aufgaben;public class A28_Klassenspiegel_Methode {
+package aufgaben.teams_aufgaben;
+
+public class A28_Klassenspiegel_Methode {
+
+    static int grade(int prozenzt) {
+        int note = 0;
+        if (prozenzt >= 0 && prozenzt <= 100) {
+            if (prozenzt > 91) {
+                note = 1;
+            } else if (prozenzt > 80) {
+                note = 2;
+            } else if (prozenzt > 66) {
+                note = 3;
+            } else if (prozenzt > 49) {
+                note = 4;
+            } else if (prozenzt > 29) {
+                note = 5;
+            } else note = 6;
+        }
+
+        return note;
+    }
+
+    static String bewertung(int grade) {
+        String answer = "";
+        switch (grade){
+            case (1):
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        int prozent = 92;
+        int note = grade(prozent);
+        System.out.println(STR."Note: \{note}\nBewertung: \{bewertung(note)}");
+    }
+
+
 }
